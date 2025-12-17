f962fc3ff429bc1719a59955fd3bcb5471183352
Aayush Raj Giri <heyayush27@gmail.com>
2025-12-17 11:10:03 +0530

    Delete fundamentals/day-02-strings-basics/divisible_by_3_and_5.py

--- fundamentals/day-02-strings-basics/divisible_by_3_and_5.py
+++
@@ -1,5 +0,0 @@
-num = int(input("Enter a number = "))
-if num % 3 == 0 and num % 5 == 0:
-    print(str(num) + " is divisible by both 3 and 5")
-else:
-    print (str(num) + " is not divisible by both 3 and 5")
