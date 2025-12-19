e791f47fe231f07b2be771e54cfd86fbc85518e9
Aayush Raj Giri <heyayush27@gmail.com>
2025-12-20 00:06:56 +0530

    Add files via upload

---
+++ fundamentals/day-06-for-loops/find_max_manual.py
@@ -0,0 +1,7 @@
+num = [27, 9, 45, 12, 3]
+max_num = num[0]
+for n in num:
+    if n > max_num:
+        max_num = n
+        
+print(max_num)
---
+++ fundamentals/day-06-for-loops/sum_of_numbers.py
@@ -0,0 +1,19 @@
+# For Loop
+num = int(input("Enter a number: "))
+sum = 0
+
+for i in range(1, num + 1):
+    sum = sum + i    
+
+print(sum)
+
+
+# While Loop
+summ = 0
+j = 1
+
+while j <= num:
+    summ = summ + j
+    j = j + 1
+
+print(summ)
