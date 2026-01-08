215fe89092eec57fa155ae4b858201d8a462134e
Aayush Raj Giri <heyayush27@gmail.com>
2026-01-08 10:52:18 +0530

    Add files via upload

---
+++ 05-oop/day-24-classes-basics/01_person_class.py
@@ -0,0 +1,14 @@
+# Create a Person class
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+p1 = Person("Aayush", 21)
+p2 = Person("Raj", 23)
+
+print(p1.name, p1.age)
+print(p2.name, p2.age)
+
+        
---
+++ 05-oop/day-24-classes-basics/02_rectangle_class.py
@@ -0,0 +1,13 @@
+# Rectangle Area
+
+class Rectangle:
+    def __init__ (self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+r1 = Rectangle(10, 5)
+print(r1.area())
+ 
